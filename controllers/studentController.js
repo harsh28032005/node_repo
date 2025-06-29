cfde2e009425af7929d60242d5167afd3c504c3c
Web-Developer-Nishant <124562481+Web-Developer-Nishant@users.noreply.github.com>
2025-06-29 23:14:57 +0530

    getIndividualStudent Api created.

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,5 +1,6 @@
 import Student from "../models/studentModel.js";
-
+import mongoose from "mongoose";
+// To create the student details in database. (POST)
 export const createStudent = async (req, res) => {
   try {
     if (!Object.keys(req.body).length) {
@@ -84,13 +85,13 @@ export const createStudent = async (req, res) => {
   }
 };
 
+// To fetch all the documents based on the filter and pagination functionality. (GET)
 export const getAllStudents = async (req, res) => {
   try {
+    // Destructuring the request query
     let { fName, age, page = 1, limit = 2 } = req.query;
 
-    // console.log(page, "page", +limit, "limit");
-    // console.log(typeof page, "page", typeof limit, "limit");
-
+    // validation
     if (isNaN(page) || page == 0) {
       return res
         .status(400)
@@ -101,11 +102,13 @@ export const getAllStudents = async (req, res) => {
         .status(400)
         .send({ status: 400, msg: "Limit must be a no and greater than 0" });
     }
-    page = +page ? +page : 1 // Ternary Operator condition ? true (Ist) : false(IInd)
-    limit = +limit ? +limit : 2 // Ternary Operator condition ? true (Ist) : false(IInd)
+    // validation
+
+    page = +page ? +page : 1; // Ternary Operator condition ? true (Ist) : false(IInd)
+    limit = +limit ? +limit : 2; // Ternary Operator condition ? true (Ist) : false(IInd)
     let skip = (+page - 1) * +limit; // (1-1)*5 = 0*5 = 0
 
-    // console.log(skip, "skip");
+    // Filter formation
     let filter = {};
 
     if (fName) {
@@ -114,15 +117,53 @@ export const getAllStudents = async (req, res) => {
     if (age) {
       filter.age = age;
     }
+    // Filter formation
 
-    // console.log(filter, "filter")
     // const getStudents = await Student.findOne(filter); // findOne query returns data in object. If no data found then it returns null.
-    const getStudents = await Student.find(filter).limit(+limit).skip(skip); // find query returns data in array. If no data found then it returns an empty array.
-    const total = await Student.countDocuments(filter)
-    return res
-      .status(200)
-      .send({ status: 200, msg: "List of all students", data: getStudents, count: total });  // no of doc/limit per
+    const getStudents = await Student.find(filter) // here we are fetching data from database.
+      .select({ _id: 1, fname: 1, lname: 1 }) // here we are selecting particular fields from database
+      .limit(+limit) // here we are applying limit on no of documents
+      .skip(skip) // here we are skipping the documents based on the page no provided.
+      .sort({ _id: -1 }); // here we are sorting the documents based on the _id field in Descending Order.
+
+    // Note:  find query returns data in array. If no data found then it returns an empty array.
+    const total = await Student.countDocuments(filter); // here we are counting the documents based on the filter applied.
+    return res.status(200).send({
+      status: 200,
+      msg: "List of all students",
+      data: getStudents,
+      count: total,
+    }); // no of doc/limit per
   } catch (err) {
     return res.status(500).send({ status: 500, msg: err });
+  }
+};
+
+// To fetch the particular document details based on the _id field (GET)
+export const getIndividualStudent = async (req, res) => {
+  try {
+    const { _id } = req.query;
+
+    // validation
+    if (!_id)
+      return res.status(400).send({ status: 400, msg: "_id is required" });
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(400).send({ status: 400, msg: "Invalid ObjectId" });
+    // validation
+
+    // const getStudent = await Student.findOne({ _id: _id }); // this will fetch the particular document. If it finds then return in the form of an object. Otherwise will return null
+
+    // const getStudent = await Student.find({ _id: _id }); // this will fetch the particular document. If it finds then return in the form of an array of object. Otherwise will return empty array like this []
+
+    const getStudent = await Student.findById(_id); // fetch the particular document based on the _id. If it finds then return in the form of an object. Otherwise will return null
+    
+    return res.status(200).json({
+      status: 200,
+      msg: "Individual Student's Details",
+      data: getStudent,
+    });
+  } catch (err) {
+    return res.status(500).send({ status: 500, msg: err.message });
   }
 };
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createStudent, getAllStudents } from "../controllers/studentController.js";
+import { createStudent, getAllStudents, getIndividualStudent } from "../controllers/studentController.js";
 const router = express.Router();
 
 router.post("/createStudent", createStudent);
 router.get("/getAllStudents", getAllStudents);
+router.get("/getIndividualStudent", getIndividualStudent);
 
 export default router;
